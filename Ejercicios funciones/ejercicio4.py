457f9972c4463299d1422995469c51b18a926dd2
Nicolas Csesztregi <nicolas.lema2@gmail.com>
2022-09-07 21:46:54 -0300

    cuarto ejercicio

---
+++ Ejercicios funciones/ejercicio4.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Fillory
+#
+# Created:     07/09/2022
+# Copyright:   (c) Fillory 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+
+    def intermedio (n1,n2):
+        suma = n1+n2
+        divide = suma/2
+
+        return divide
+    print (intermedio (12,50))
+
+
+if __name__ == '__main__':
+    main()
