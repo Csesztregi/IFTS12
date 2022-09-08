d0a8ef3b50db471daa416706a12f2e5bef1f02b8
Nicolas Csesztregi <nicolas.lema2@gmail.com>
2022-09-07 21:45:18 -0300

    segundo ejercicio

---
+++ Ejercicios funciones/ejercicio2.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      Fillory
+#
+# Created:     06/09/2022
+# Copyright:   (c) Fillory 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+    def area_circulo(n1):
+        area= (f"el area es: {3.14159 * n1}cm2")
+        return area
+    print (area_circulo(361))
+if __name__ == '__main__':
+    main()
