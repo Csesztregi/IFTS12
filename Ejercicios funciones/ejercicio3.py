fba31849ba3402891193a76fb0428593ec2feb88
Nicolas Csesztregi <nicolas.lema2@gmail.com>
2022-09-07 21:46:07 -0300

    tercer ejercicio

---
+++ Ejercicios funciones/ejercicio3.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Fillory
+#
+# Created:     07/09/2022
+# Copyright:   (c) Fillory 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+    def relacion(n1,n2):
+        if n1>n2:
+            return (1)
+
+        if  n1<n2:
+            return (-1)
+        else:
+            return (0)
+
+    print (relacion(-3,5))
+if __name__ == '__main__':
+    main()
