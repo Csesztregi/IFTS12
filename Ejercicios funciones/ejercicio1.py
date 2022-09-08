40b6beaf796dac921670e6c5a57fede265ebe1d0
Nicolas Csesztregi <nicolas.lema2@gmail.com>
2022-09-07 21:43:04 -0300

    Cree carpeta para ejercicios de funciones y agregue el primer ejercicio realizado

---
+++ Ejercicios funciones/ejercicio1.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Fillory
+#
+# Created:     06/09/2022
+# Copyright:   (c) Fillory 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+
+
+    def area_rectangulo(n1,n2):
+        area = n1 * n2
+        return area
+    print (area_rectangulo(2,3))
+
+
+
+
+
+if __name__ == '__main__':
+    main()
